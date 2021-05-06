84ca30cb3af7fe910d2bc6660f126cd2d0a5e41b
George <giorgi.batsiashvili@geolab.edu.ge>
2021-05-06 11:08:46 +0400

    chore: replace german Nationality Code

--- src/parsers/parseState.js
+++ src/parsers/parseState.js
@@ -8,13 +8,17 @@ module.exports = function parseState(source) {
   let updatedSource = source
   if (updatedSource && updatedSource.includes('GE0')) {
     updatedSource = updatedSource.replace('GE0', 'GEO')
+  } else if (updatedSource === 'D<<' ) {
+    updatedSource = 'D'
   }
 
   source = cleanText(updatedSource);
   let state = STATES[updatedSource];
+  
   if (!state) {
     throw new Error(`invalid state code: ${source}`);
   }
+
   return {
     value: source,
     start: 0,
